f30bd3c10a41228228edbeebd1e0f218cb06cb08
Gautham Gujjula <gauthamgujjula@utexas.edu>
2020-06-14 17:53:07 +0000

    See change in txt file

--- include/utils.h
+++ include/utils.h
@@ -30,9 +30,7 @@
 
 #define MIN(a,b) ((b) < (a) ? (b) : (a))
 #define MAX(a,b) ((b) > (a) ? (b) : (a))
-#if TAS_TARGET_ARCH==x86_64
-  #define MEM_BARRIER() __asm__ volatile("" ::: "memory")
-#else
+#define MEM_BARRIER() __asm__ volatile("" ::: "memory")
 #endif
 #define STATIC_ASSERT(COND,MSG) typedef char static_assertion_##MSG[(COND)?1:-1]
 #define LIKELY(x) __builtin_expect((x),1)
